46547e5818cb5fe327995564bd285b753bcda9c8
iPaldy <muhammadrifaldy333@gmail.com>
2021-02-19 21:03:46 +0700

    menyelesaikan fungsi login dan menghapus , table customer_user hanya berisi data perusahaan, untuk pembuatan akun ikut dengan table user

--- application/controllers/Administrator.php
+++ application/controllers/Administrator.php
@@ -5,6 +5,7 @@ class Administrator extends CI_Controller
 {
     public function index()
     {
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
         $data['title'] = 'Administrator / Tikicorp';
 
         $this->load->view('templates/header', $data);
--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -29,13 +29,38 @@ class Auth extends CI_Controller
 
         // ambil data user dari database
         $user = $this->db->get_where('user', ['email' => $email])->row_array();
-        $customer_user = $this->db->get_where('customer_user', ['email' => $email])->row_array();
 
+        // jika user ada
         if ($user) {
-            // user ada
+            // jika user aktif
+            if ($user['is_active'] == 1) {
+                // cek password
+                if (password_verify($password, $user['password'])) {
+                    $data = [
+                        'email' => $user['email'],
+                        'role_id' => $user['role']
+                    ];
+                    $this->session->set_userdata($data);
+                    redirect('administrator');
+                } else {
+                    $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Wrong password!</div>');
+                    redirect('auth');
+                }
+            } else {
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">This email has not been activated!</div>');
+                redirect('auth');
+            }
         } else {
             $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Email is not registered!</div>');
             redirect('auth');
         }
+    }
+
+    public function logout()
+    {
+        $this->session->unset_userdata('email');
+        $this->session->unset_userdata('role_id');
+
+        redirect('auth');
     }
 }
